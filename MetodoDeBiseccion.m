c63596ce87c0a82c0a0d51163efc937d2c2322d6
Apuntes-De-Un-Ingeniero <juank2001estevez@gmail.com>
2022-04-28 10:16:54 -0500

    Actualizando el método de bisección.

--- MetodoDeBiseccion.m
+++ MetodoDeBiseccion.m
@@ -1,71 +1,33 @@
-clc; clear all;
+function biseccion()
 
-f=@(x) cos(x)+x; %Función dependiente de x.
-fplot(f,'k-','LineWidth',2); %Grafica la función de color negro y grosor 2
-title(func2str(f)); hold on; grid on; %Título de la función.
-line([-5 5],[0 0],'Color','k','LineStyle','--'); %Marca el eje X.
-line([0 0],[-5 5],'Color','k','LineStyle','--') %Marca el eje Y.
+clc;
 
-xl=-2; %Límite inferior.
-xu=0; %Límite superior.
-fxl=f(xl); %Punto en Y para el límite inferior.
-fxu=f(xu); %Punto en Y para el límite superior.
-Niter=100; %Número de iteraciones. Recomiendo usar 100.
-Tol=0.01; %Tolerancia para el criterio de convergencia a superar o igualar (%)
+f = input('Ingrese la función (en comilla sencilla):  ');
+f = inline(f);
 
-%% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~Algoritmo~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~%
+a = input('Ingrese el límite inferior del intervalo:  ');
+b = input('Ingrese el límite superior del intervalo:  ');
+tolerancia = input('Ingrese el error máximo permitido:  ');
 
-x = linspace(xl,xu,100);
-ylim = [min(f(x)),max(f(x))];
-if fxl*fxu > 0 %Esta propiedad es la que hace que éste sea un método cerrado.
-    error('No hay una raíz en ese intervalo!'); 
+if (f(a)*f(b) > 0)
+    error('No se cumple el Teorema del valor medio');
 end
 
-obj=plot([xl,xu],[fxl,fxu],'ko','markerfacecolor','b'); %Grafica los límites como puntos
-fprintf('Presiona una tecla para continuar\n'); pause;
+contador = 0;
 
-for i = 1:Niter
-    % Biseccionar
-    xr(i)=(xl(i)+xu(i))/2; %Calcula el punto medio actual.
-    fxr(i)=f(xr(i)); %Evalua la función en el punto medio actual.
-    
-    if f(xr(i))*f(xl(i)) > 0 %Si esta condición se cumple, la raíz NO está entre xl y xr
-        xl(i+1) = xr(i); %El punto medio es el nuevo límite inferior.
-        xu(i+1) = xu(i); %El límite superior se mantiene igual.
-        fxl(i+1)=f(xl(i+1));
-        fxu(i+1)=f(xu(i+1));
-    elseif f(xr(i))*f(xu(i)) > 0 %Si esta condición se cumple, la raíz NO está entre xu y xr
-        xu(i+1) = xr(i); %El punto medio es el nuevo límite superior.
-        xl(i+1) = xl(i); %El límite inferior se mantiene igual.
-        fxl(i+1)=f(xl(i+1));
-        fxu(i+1)=f(xu(i+1));
+while (abs(b - a) > tolerancia)
+    semilla = (a + b)/2;
+    if (f(a)*f(semilla) < 0)
+        b = semilla;
     end
-    
-    xr(i+1)=(xu(i+1)+xl(i+1))/2; %Actulizamos el punto medio y su punto en Y
-    fxr(i+1)=f(xr(i+1));
-    Error(i+1)=abs((xr(i+1)-xr(i))/xr(i+1))*100; %Calcula el error relativo actual
-    
-    %Grafica nuevos puntos
-    obj1 = plot(xr(i),fxr(i),'ko','markerfacecolor','r'); %Grafica el punto medio actual.
-    obj2 = plot([xr(i),xr(i)],ylim,'r--'); %Grafica una línea vertical que pasa por el punto medio actual
-    fprintf('Presiona una tecla para continuar\n'); pause;
-    
-    % Grafica el nuevo intervalo
-    delete(obj1); delete(obj2); %Borra el punto medio actual y su línea de apoyo
-    set(obj,'markerfacecolor','w'); %Vuelve blanco el punto que ya no nos sirve
-    obj=plot([xl(i+1),xu(i+1)],[fxl(i+1),fxu(i+1)],'ko','markerfacecolor','b'); %Actualiza los límites
-    fprintf('Presiona una tecla para continuar\n'); pause;
-    
-    if Error(i+1) < Tol %Si el error relativo es menor a la tolerancia exigida, se acaba el ciclo.
-        fprintf('Convergencia!\n'); 
-        obj1 = plot(xr(i),fxr(i),'ko','markerfacecolor','r');
-        obj2 = plot([xr(i),xr(i)],ylim,'r--');
-        break;
+    if (f(semilla)*f(b) < 0)
+        a = semilla;
     end
+    if (abs(f(semilla)) < eps)
+        fprintf('\nLa raíz es: %f\n\n', semilla);
+        return
+    end
+    contador = contador + 1;
+    fprintf('\nIteración %d, intervalo [%f, %f]', contador, a, b);
 end
-
-%% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Resultados~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~%
-
-Raiz=xr(end)
-Error_relativo=Error(end)
-Numero_iter=i+1
+fprintf('\n\n');
