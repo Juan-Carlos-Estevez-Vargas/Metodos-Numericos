def56d3854c1c56961f7fc8462241f56a7be6def
Apuntes-De-Un-Ingeniero <juank2001estevez@gmail.com>
2022-05-28 12:34:13 -0500

    Optimización método de Newton-Raphson.

--- 5. Metodo Newton-Raphson/NewtonRaphson.m
+++ 5. Metodo Newton-Raphson/NewtonRaphson.m
@@ -1,17 +1,18 @@
-function NewtonRaphson
+function NewtonRaphson(x0)
 syms x;
-f = input('Digite la función deseada ');
+f = input('ingrese nuevamente la función ');
 df = diff(f);
-x0 = input('Digite el valor inicial ');
+disp(df);
 n = input('Digite el número de ieraciones ');
 tol = input('Ingrese la tolerancia ');
 
 for k=1: n
     x1 = x0 - subs(f, x0)/subs(df, x0);
     if(abs(x1 - x0) < tol)
-        fprintf('x%d=%f es un aproximación de una raíz\n', k, x1);
+        fprintf('\nx%d=%f es un aproximación de una raíz\n', k, x1);
+        disp('\n')
         return 
     end
-    fprintf('x%d=%f\n', k, x1);
+    fprintf('x%d=%f\n', k, x1); 
     x0 = x1;
 end
