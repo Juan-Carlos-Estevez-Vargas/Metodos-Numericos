76e99c92481b648cfcdff1de1ce9a39f952276ed
Apuntes-De-Un-Ingeniero <juank2001estevez@gmail.com>
2022-06-01 15:10:14 -0500

    Método de Interpolación Simple o Polinómica.

---
+++ 7. Interpolacion Simple/InterpolacionSimple.m
@@ -0,0 +1,25 @@
+%Calcula una tabla de valores de ini a fin con un intervalo de h para una determinada ecuacion
+clc
+y = [];
+x = [];
+n = input('Introduzca la cantidad de puntos: ');
+
+disp ();
+disp ('A continuación, ingrese los valor de x')
+for i=1:n
+    a = input('Ingrese un valor númerico ');
+    x = [x, a];
+end
+
+disp ();
+disp ('A continuación, ingrese los valor de y')
+for i=1:n
+    a = input('Ingrese un valor númerico ');
+    y = [y, a];
+end
+
+w = polyfit(x, y, n-1);
+xx = linspace(0, 15, 100);
+yy = polyval(w, xx);
+hold on, grid on, plot(xx, yy);
+w
